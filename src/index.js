e1688edbfaf7b799a77b4044f659f0c1e5cd64e5
benbousquet <bbousque@asu.edu>
2021-06-17 00:05:00 -0700

    added pg conn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import pool from "./database";
 
 const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("test");
+  pool
+    .query("SELECT NOW() as now")
+    .then((dbres) => {
+      res.send(dbres.rows[0]);
+    })
+    .catch((e) => console.error(e.stack));
 });
 
-app.listen(2000, () => {
-  console.log("listening on http://localhost:2000");
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
 });
