dc6ae48fa151810046e327a5547df2d5febf5ed2
benbousquet <bbousque@asu.edu>
2021-06-16 21:12:11 -0700

    added basic route

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-let test = () => {
-  console.log("test");
-};
+import express from "express";
 
-test();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+app.listen(2000, () => {
+  console.log("listening on http://localhost:2000");
+});
